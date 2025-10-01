81ae3bd1a7717cdc05cfb09c721024f1c5753195
Hayden Delgado <20595942@lab219-b02.staff.ad.curtin.edu.au>
2025-10-01 23:32:06 +0800

    Trained model

--- train.py
+++ train.py
@@ -1,3 +1,12 @@
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")
+print(model.model)
+
+# model.train(
+#     data='Dataset/data.yaml',
+#     epochs=20,
+#     imgsz=640,
+#     batch=16,
+#     name="building_numbers"
+# )
